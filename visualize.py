baf83ad855f06d07ef59e8ad0079df0bdd401b7a
Enmin Zhou <zhouem14@gmail.com>
2019-04-13 16:52:27 -0700

    Change Data structure; Add multiple graphs

--- visualize.py
+++ visualize.py
@@ -14,40 +14,42 @@ def randomColor(usedColor):
 	return '#'+color
 
 
-def makeTitle():
-	return 'Value vs Layer'
+def makeTitle(xlabel, ylabel):
+	return '%s vs %s' % (xlabel, ylabel)
 
 
 def draw(data):
 	plt.figure(figsize=(12, 12))
 	usedColor =[]
-	maxValue = 0
-	maxLayer = 0
-	for numOfNeuron in data.keys():
-		color = randomColor(usedColor)
-		usedColor.append(color)
-		numOfLayerDict = data[numOfNeuron]
-		if maxValue < max(numOfLayerDict.values()):
-			maxValue = max(numOfLayerDict.values())
-		if maxLayer < max(numOfLayerDict.keys()):
-			maxLayer = max(numOfLayerDict.keys())
-		# print([numOfLayerDict[layer] for layer in numOfLayerDict.keys()])
-		plt.plot([layer for layer in numOfLayerDict.keys()], [numOfLayerDict[layer] for layer in numOfLayerDict.keys()], label=str(numOfNeuron), color=color)
-	plt.xlabel('layer')
-	plt.ylabel('value')
-	plt.legend(loc='upper left')
-	plt.xticks(np.arange(0, maxLayer, step=1))
-	plt.yticks(np.arange(0, maxValue, step=1))
-	title = makeTitle()
-	plt.title(title)
-	plt.show()
-	plt.savefig(title)
+	neuronList = []
+	layerList = []
+	colorPair = {}
+	numOfFigures = len(list(data.values())[0])
+	for keys in data.keys():
+		if keys[0] not in neuronList:
+			neuronList.append(keys[0])
+		if keys[1] not in layerList:
+			layerList.append(keys[1])
+	for i in range(0, numOfFigures):
+		plt.subplot(numOfFigures * 100 + 10 + i + 1)
+		for numOfNeuron in neuronList:
+			colorPair[numOfNeuron] = randomColor(usedColor)
+			usedColor.append(colorPair[numOfNeuron])
+			plt.plot([key[1] if key[0]==numOfNeuron else None for key in data.keys()],
+						[data[key][i] if key[0]==numOfNeuron else None for key in data.keys()],
+						label=str(numOfNeuron), color=colorPair[numOfNeuron])
+			plt.xlabel('Layer')
+			plt.ylabel('Value%s' % i)
+			plt.legend(loc='upper left')
+			plt.xticks(np.arange(0, max(layerList), step=1))
+			plt.yticks(np.arange(0, 100, step=5))
+			title = makeTitle('Value%s'%i, 'Layer')
+			plt.savefig(title)
 
 
 if __name__ == '__main__':
-	data = {200: {2: 10, 4: 20, 8: 30, 16: 40},
-			400: {2: 15, 4: 25, 8: 35, 16: 45},
-			800: {2: 18, 4: 28, 8: 38, 16: 48},
-			1000: {2: 22, 4: 32, 8: 42, 16: 52},
-			}
+	data = {(200,2): [10, 20], (200,4): [20,30], (200,8): [30,40], (200,16):[40,50],
+			(400,2): [15,25], (400,4): [25,35], (400,8): [35,45], (400,16): [45,55],
+			(800,2): [18,28], (800,4): [28,38], (800,8): [38,48], (800,16): [48,58],
+			(1000,2): [22,32], (1000,4): [32,42], (1000,8): [42,52], (1000,16): [52,62]}
 	draw(data)
