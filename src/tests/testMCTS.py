38c65a0d8c69a05e853918d84ffbe782c55c9195
besth <zhaoqy1997@outlook.com>
2019-04-24 15:52:24 -0700

    simple test for rollout

--- src/tests/testMCTS.py
+++ src/tests/testMCTS.py
@@ -3,7 +3,7 @@ import numpy as np
 from ddt import ddt, data, unpack
 from anytree import AnyNode as Node
 from algorithms.mcts import CalculateScore, SelectChild, Expand, RollOut, backup
-from simple1DEnv import TransitionFunction, RewardFunction
+from simple1DEnv import TransitionFunction, RewardFunction, Terminal
 
 
 @ddt
@@ -14,11 +14,15 @@ class TestMCTS(unittest.TestCase):
         bound_high = 7
         self.transition = TransitionFunction(bound_low, bound_high)
 
+        self.action_space = [-1, 1]
+        self.num_action_space = len(self.action_space)
+
         step_penalty = -1
         catch_reward = 1
-        target_state = bound_high
+        self.target_state = bound_high
+        self.isTerminal = Terminal(self.target_state)
 
-        self.reward = RewardFunction(step_penalty, catch_reward, target_state)
+        # self.reward = RewardFunction(step_penalty, catch_reward, self.target_state)
 
         self.c_init = 1
         self.c_base = 0
@@ -28,50 +32,76 @@ class TestMCTS(unittest.TestCase):
         level1_0_state = self.transition(init_state, action=0)
         # print("level 1 state", level1_0_state)
         level1_1_state = self.transition(init_state, action=1)
+        self.default_action_prior = 0.5
 
         self.root = Node(id={1: init_state}, num_visited=1, sum_value=0, action_prior=self.default_action_prior, is_expanded=True)
         self.level1_0 = Node(parent=self.root, id={0: level1_0_state}, num_visited=2, sum_value=5, action_prior=self.default_action_prior, is_expanded=False)
         self.level1_1 = Node(parent=self.root, id={1: level1_1_state}, num_visited=3, sum_value=10, action_prior=self.default_action_prior, is_expanded=False)
 
-        self.expand = Expand(self.num_action_space, self.transition)
+        self.expand = Expand(self.num_action_space, self.transition, self.isTerminal)
 
-    @data((0, 0, 0, 1, 0), (1, 0, 0, 1, np.log(2)), (1, 1, 0, 1, np  
+    # @data((0, 0, 0, 1, 0), (1, 0, 0, 1, np.log(2)), (1, 1, 0, 1)  )
+    # @unpack
+    # def testCalculateScore(self, parent_visit_number, self_visit_number, sum_value, action_prior, true_score):
+    #     # curr_node = 
+    #     score = calculate_score(exploration_rate, parent_visit_number, self_visit_number, mean_value, action_prior)
+    #     gt_score = np.sqrt(2) / 4 + 3
+    #     self.assertEqual(score, gt_score)
+
+    # @data()
+    # @unpack
+    # def testSelectChild(self, firstChildVisited, firstChildSumValue, secondChildVisited, secondChildSumValue):
+    #     child = self.selectChild(self.root)
+    #     child_id_action = list(child.id.keys())[0]
+    #     gt_score_0 = 5 / 2 + 1.0 * 0.5 * 1 / (1 + 2)
+    #     gt_score_1 = 10 / 3 + 1.0 * 0.5 * 1 / (1 + 3)
+    #     gt_action = np.argmax([gt_score_0, gt_score_1])
+
+    #     self.assertEqual(gt_action, child_id_action)
+
+    # def testExpand(self):
+    #     # test whether children have been created with the correct values.
+    #     curr_node = self.level1_0
+
+    #     new_curr_node = self.expand(self.level1_0)
+
+    #     children = new_curr_node.children
+
+    #     child_0 = children[0]
+    #     child_1 = children[1]
+    #     child_0_cal_state = list(child_0.id.values())[0]
+    #     self.assertEqual(child_0_cal_state, 1)
+
+    #     child_1_cal_state = list(child_1.id.values())[0]
+    #     self.assertEqual(child_1_cal_state, 3)
+
+    @data((3, 3, 0.125), (2, 4, 0.25))
     @unpack
-    def testCalculateScore(self, parent_visit_number, self_visit_number, sum_value, action_prior, true_score):
-        # curr_node = 
-        score = calculate_score(exploration_rate, parent_visit_number, self_visit_number, mean_value, action_prior)
-        gt_score = np.sqrt(2) / 4 + 3
-        self.assertEqual(score, gt_score)
+    def testRollout(self, max_rollout_step, init_state, gt_sum_value):
+        max_iteration = 1000
 
-    @data()
-    @unpack
-    def testSelectChild(self, firstChildVisited, firstChildSumValue, secondChildVisited, secondChildSumValue):
-        child = self.selectChild(self.root)
-        child_id_action = list(child.id.keys())[0]
-        gt_score_0 = 5 / 2 + 1.0 * 0.5 * 1 / (1 + 2)
-        gt_score_1 = 10 / 3 + 1.0 * 0.5 * 1 / (1 + 3)
-        gt_action = np.argmax([gt_score_0, gt_score_1])
+        target_state = 6
+        isTerminal = Terminal(target_state)
 
-        self.assertEqual(gt_action, child_id_action)
+        catch_reward = 1
+        step_penalty = 0
+        reward_func = RewardFunction(step_penalty, catch_reward, isTerminal)
 
-    def testExpand(self):
-        # test whether children have been created with the correct values.
-        curr_node = self.level1_0
+        rollout_policy = lambda state: np.random.choice(self.action_space)
+        leaf_node = Node(id={1: init_state}, num_visited=1, sum_value=0, action_prior=self.default_action_prior, is_expanded=True)
+        rollout = RollOut(rollout_policy, max_rollout_step, self.transition, reward_func, isTerminal)
+        stored_reward = []
+        for curr_iter in range(max_iteration):
+            print(curr_iter, rollout(leaf_node))
+            stored_reward.append(rollout(leaf_node))
+        
+        calc_sum_value = np.mean(stored_reward)
+        print(stored_reward)
+        self.assertAlmostEqual(gt_sum_value, calc_sum_value)
 
-        new_curr_node = self.expand(self.level1_0)
 
-        children = new_curr_node.children
 
-        child_0 = children[0]
-        child_1 = children[1]
-        child_0_cal_state = list(child_0.id.values())[0]
-        self.assertEqual(child_0_cal_state, 1)
-
-        child_1_cal_state = list(child_1.id.values())[0]
-        self.assertEqual(child_1_cal_state, 3)
-
-    def testRollout(self):
-        pass
+        # init_state = self.rollout_init_state
 
 
 
