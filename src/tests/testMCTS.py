4067cb6f39b95b28266def1120c8ab799181e15d
besth <zhaoqy1997@outlook.com>
2019-04-24 19:51:45 -0700

    Added testcases for expand and backup

--- src/tests/testMCTS.py
+++ src/tests/testMCTS.py
@@ -5,7 +5,7 @@ import numpy as np
 from ddt import ddt, data, unpack
 from anytree import AnyNode as Node
 # from MultiAgent-Chasing
-from algorithms.mcts import CalculateScore, SelectChild, Expand, RollOut, backup
+from algorithms.mcts import CalculateScore, SelectChild, Expand, RollOut, backup, ActionPriorFunction
 from simple1DEnv import TransitionFunction, RewardFunction, Terminal
 
 
@@ -19,6 +19,8 @@ class TestMCTS(unittest.TestCase):
 
         self.action_space = [-1, 1]
         self.num_action_space = len(self.action_space)
+        self.action_prior_func = ActionPriorFunction(self.action_space)
+
 
         step_penalty = -1
         catch_reward = 1
@@ -43,7 +45,7 @@ class TestMCTS(unittest.TestCase):
         self.level1_0 = Node(parent=self.root, id={0: level1_0_state}, num_visited=2, sum_value=5, action_prior=self.default_action_prior, is_expanded=False)
         self.level1_1 = Node(parent=self.root, id={1: level1_1_state}, num_visited=3, sum_value=10, action_prior=self.default_action_prior, is_expanded=False)
 
-        self.expand = Expand(self.num_action_space, self.transition, self.isTerminal)
+        self.expand = Expand(self.action_prior_func, self.transition, self.isTerminal)
 
     @data((0, 1, 0, 1, 0), (1, 1, 0, 1, np.log(3)/2), (1, 1, 1, 1, 1 + np.log(3)/2))
     @unpack
@@ -56,7 +58,7 @@ class TestMCTS(unittest.TestCase):
 
     @data((1, 1, 1, 1, 100))
     @unpack
-    def testVisitNumEffectsOnSelectChild(self, firstChildVisited, firstChildSumValue, secondChildVisited, secondChildSumValue, maxSelectTimes):
+    def testVisitValueEffectsOnSelectChild(self, firstChildVisited, firstChildSumValue, secondChildVisited, secondChildSumValue, maxSelectTimes):
         curr_node = Node(num_visited = 0)
         first_child = Node(parent = curr_node, id = 'first', num_visited = firstChildVisited, sum_value = firstChildSumValue, action_prior = 0.5, is_expanded = False)
         second_child = Node(parent = curr_node, id = 'second', num_visited = secondChildVisited, sum_value = secondChildSumValue, action_prior = 0.5, is_expanded = False)
@@ -69,7 +71,7 @@ class TestMCTS(unittest.TestCase):
 
     @data((1, 1, 1, 1, 100))
     @unpack
-    def testVisitValueEffectsOnSelectChild(self, firstChildVisited, firstChildSumValue, secondChildVisited, secondChildSumValue, maxSelectTimes):
+    def testVisitNumEffectsOnSelectChild(self, firstChildVisited, firstChildSumValue, secondChildVisited, secondChildSumValue, maxSelectTimes):
         curr_node = Node(num_visited = 1)
         first_child = Node(parent = curr_node, id = 'first', num_visited = firstChildVisited, sum_value = firstChildSumValue, action_prior = 0.5, is_expanded = False)
         second_child = Node(parent = curr_node, id = 'second', num_visited = secondChildVisited, sum_value = secondChildSumValue, action_prior = 0.5, is_expanded = False)
@@ -79,29 +81,26 @@ class TestMCTS(unittest.TestCase):
             self.assertTrue(new_child.id != old_child_id)
             new_child.num_visited += 1
             old_child_id = new_child.id
-    
-    @unittest.skip  
-    @data(())
+     
+    @data((3, True, [2, 4]), (0, True, [0, 1]), (7, False, None))
     @unpack
-    def testExpand(self, ):
-        # test whether children have been created with the correct values.
-        curr_node = self.level1_0
-        new_curr_node = self.expand(self.level1_0)
+    def testExpand(self, state, has_children, child_states):
 
-        children = new_curr_node.children
-        child_0 = children[0]
-        child_1 = children[1]
-        child_0_cal_state = list(child_0.id.values())[0]
-        self.assertEqual(child_0_cal_state, 1)
+        leaf_node = Node(id = {1: state}, num_visited = 1, sum_value = 1, action_prior = 0.5, is_expanded = False)
+        expanded_node = self.expand(leaf_node)
 
-        child_1_cal_state = list(child_1.id.values())[0]
-        self.assertEqual(child_1_cal_state, 3)
+        calc_has_children = (len(expanded_node.children) != 0)
+        self.assertEqual(has_children, calc_has_children)
 
+        for child_index, child in enumerate(expanded_node.children):
+            cal_child_state = list(child.id.values())[0]
+            gt_child_state = child_states[child_index]
+            self.assertEqual(gt_child_state, cal_child_state)
 
     @data((4, 3, 0.125), (3, 4, 0.25))
     @unpack
     def testRollout(self, max_rollout_step, init_state, gt_sum_value):
-        max_iteration = 10000
+        max_iteration = 1000
 
         target_state = 6
         isTerminal = Terminal(target_state)
@@ -120,6 +119,20 @@ class TestMCTS(unittest.TestCase):
         calc_sum_value = np.mean(stored_reward)
 
         self.assertAlmostEqual(gt_sum_value, calc_sum_value, places=1)
+
+    @data((5, [3,4], [2,1], [8,9], [3,2]))
+    @unpack
+    def testBackup(self, value, prev_sum_values, prev_visit_nums, new_sum_values, new_visit_nums):
+        node_list = []
+        for prev_sum_value, prev_visit_num in zip(prev_sum_values, prev_visit_nums):
+            node_list.append(Node(id = {1: 4}, num_visited = prev_visit_num, sum_value = prev_sum_value, action_prior = 0.5, is_expanded = False))
+
+        backup(value, node_list)
+        cal_sum_values = [node.sum_value for node in node_list]
+        cal_visit_nums = [node.num_visited for node in node_list]
+
+        self.assertTrue(np.all(cal_sum_values == new_sum_values))
+        self.assertTrue(np.all(cal_visit_nums == new_visit_nums))
 
 
 if __name__ == "__main__":
